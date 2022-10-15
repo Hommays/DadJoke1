92531039da6b8bbbb94f53af9f8dd9ae26ecf2e1
Hommays <hommays1995@gmail.com>
2022-10-15 16:16:48 +0300

    Refresh button demo version 1

--- app/src/main/java/com/example/dadjoke1/BlankFragment2.kt
+++ app/src/main/java/com/example/dadjoke1/BlankFragment2.kt
@@ -6,6 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.w3c.dom.Text
+import retrofit2.http.GET
+import retrofit2.http.Headers
 
 class BlankFragment : Fragment() {
 
@@ -14,6 +21,7 @@ class BlankFragment : Fragment() {
     }
 
     private lateinit var viewModel: FragmentViewModel
+    private val TAG = this::class.simpleName!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +30,39 @@ class BlankFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_blank2, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+       super.onViewCreated(view, savedInstanceState)
+        val randomJokeButton: Button = view.findViewById(R.id.buttonJoke)
+        val randomJokeText: Text = view.findViewById(R.id.textOfJoke)
+        randomJokeButton.setOnClickListener{
+            viewModel.dadJokeService.singleRandomDadJoke()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{ response, error ->
+                    if (error!= null){
+                        randomJokeText.textContent = response?.joke ?: getString(R.string.error_try_again)
+                    }else{
+                        randomJokeText.textContent = response?.joke ?: getString(R.string.cant_find_joke)
+                    }
+                }
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(FragmentViewModel::class.java)
     }
 
+}
+
+data class DadJokeResponse(
+    val id: String,
+    val joke: String,
+    val status: Int
+)
+interface DadJokeService{
+    @GET("/")
+    @Headers(
+    )
+    fun singleRandomDadJoke(): Single<DadJokeResponse>
 }
--- app/src/main/java/com/example/dadjoke1/FragmentViewModel.kt
+++ app/src/main/java/com/example/dadjoke1/FragmentViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.dadjoke1
 
 import androidx.lifecycle.ViewModel
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
-class FragmentViewModel : ViewModel() {
-
+open class FragmentViewModel : ViewModel() {
+    val dadJokeService by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://dadjokes.io/documentation/endpoints/random-jokes")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(DadJokeService::class.java)
+    }
 }
